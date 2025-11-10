2c55e97e9c2d9e819a246292c025273d50a6655f
Brian Le <brianle17360@gmail.com>
2025-11-10 12:48:24 -0800

    bot can now talk with users

--- src/cogs/message.py
+++ src/cogs/message.py
@@ -1,13 +1,84 @@
 import discord
 from discord.ext import commands
+import re
+from src.utils.kobold import KoboldClient
 
 class Message(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+        self.kobold_client = KoboldClient()
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         print(f"{message.author.name}: {message.content}")
+
+        # prevent replies to itself
+        if message.author.id == self.bot.user.id or message.author.bot:
+            return
+        
+        if len(message.content) == 0:
+            return
+        
+        user_msg = self.replace_user_mentions(message.content)
+
+        if "glados" not in user_msg.lower():
+            return
+        
+        # Have glados reply if mentioned
+        response = self.kobold_client.get_response(user_msg, message.author.name)
+        response = self.trim_incomplete_sentence(response)
+
+        await message.channel.send(response)
+
+
+    def replace_user_mentions(self, message: str) -> str:
+        """Replaces @<user> mentions with their discord usernames"""
+        new_msg = ""
+        mention_pattern = r"<@\d+>"
+        result: list[str] = re.findall(mention_pattern, message)
+
+        for x in result:
+            # x is <@1234...>
+            if x in message:
+                id_pattern = r"<@(\d+)>"
+
+                # extract just the number, omit <> and @
+                result = re.findall(id_pattern, x)
+                id = int(result[0])
+                user = self.bot.get_user(id)
+                if not user:
+                    continue
+                name = user.name
+                new_msg = message.replace(x, name)
+        
+        # return old message if nothing got changed
+        if not new_msg:
+            return message
+        
+        # otherwise, return new message.
+        return new_msg
+    
+    def trim_incomplete_sentence(self, response: str) -> str:
+        """
+        Trims any incomplete/broken sentence in a response.
+
+        Parameters:
+            response (str): The LLM response, that may consist of multiple sentences.
+        Return:
+            str: The LLM response free of any broken/incomplete sentence.
+        """
+
+        # for if the llm generated a single sentence w/ no punctuations
+        if not any(char in response for char in '.!?'):
+            return response
+
+        # trim sentence to the last punctuation
+        i = len(response) - 1
+        while response[i] not in ('.', '!', '?'):
+            i -= 1
+        
+        return response[:i+1]
+
 
 def setup(bot: discord.Bot):
     bot.add_cog(Message(bot))
---
+++ src/utils/kobold.py
@@ -0,0 +1,90 @@
+'''
+This module contains classes and methods for interacting with the koboldcpp server.
+'''
+import requests
+from src.utils.prompts.helper import system_prompt
+
+class KoboldClient:
+    """
+    The class that handles kobold-related tasks
+    """
+    def __init__(self):
+        self.base_url = "http://brian-le-llm.duckdns.org:5500"
+        self.max_length = 150
+        self.temperature = 0.6
+        self.stop_sequence = ["\n", "</s>[INST]", "[/INST]"]
+        self.api_key = "llmgroup9"
+
+        self.chat_logs: list[str] = []
+
+
+    def generate_prompt(self) -> str:
+        """
+        Generates a complete prompt for the kobold prompt body.
+
+        Returns:
+            str: The prompt as a string.
+        """
+        prompt = f"{system_prompt}\n\n"
+        prompt += f"[Chat logs:]\n"
+
+        for message in self.chat_logs:
+            prompt += message + "\n"
+        
+        # Now we have the LLM generate text for GlaDOS
+        prompt += "GlaDOS:"
+
+        return prompt
+
+
+    def generate_request_body(self, prompt: str) -> dict:
+        """
+        Generates request body for post requesting to kobold's generation endpoint.
+
+        Args:
+            prompt (str): The prompt to be given as input to the LLM.
+        Returns:
+            dict: The json body as a dict.
+        """
+
+        body = {
+            "max_length": self.max_length,
+            "temperature": self.temperature,
+            "stop_sequence": self.stop_sequence,
+            "prompt": prompt
+        }
+
+        return body
+
+
+    def get_response(self, user_msg: str, user_name: str) -> str:
+        """
+        Get a response from kobold server.
+
+        Args:
+            user_msg (str): User's message.
+            user_name (str): User's name.
+        Returns:
+            str: LLM response.
+        """
+        headers = {"Authorization": f"Bearer {self.api_key}"}
+        url = self.base_url + "/api/v1/generate"
+
+        # add user's message to chat history
+        self.chat_logs.append(f"{user_name}: {user_msg}")
+
+        prompt = self.generate_prompt()
+        body = self.generate_request_body(prompt)
+
+        try:
+            result = requests.post(url=url, headers=headers, json=body)
+        except Exception as e:
+            raise Exception("Error communicating with Kobold server. Are you connected and is the server running?")
+
+        response: str = result.json()["results"][0]["text"]
+        response = response.strip()
+
+        # add glados's message to the history as well.
+        self.chat_logs.append(f"GlaDOS: {response}")
+        return response
+
